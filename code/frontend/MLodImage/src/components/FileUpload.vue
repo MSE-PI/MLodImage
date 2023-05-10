6c40ce4d3e2eb40d4584142dcfb7d469ad754582
Andrea Petrucci <andrea.petrucci@edu.hefr.ch>
2023-05-10 22:54:04 +0200

    change favicon

--- code/frontend/MLodImage/src/components/FileUpload.vue
+++ code/frontend/MLodImage/src/components/FileUpload.vue
@@ -63,7 +63,7 @@ function handleClickDeleteFile() {
   border-radius: 8px;
   box-shadow: rgba(60, 64, 67, 0.3) 0 1px 2px 0,
   rgba(60, 64, 67, 0.15) 0 1px 3px 1px;
-  font-size: 12px;
+  font-size: 16px;
   background-color: #fff;
   line-height: 1.5;
 }
