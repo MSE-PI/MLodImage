c16251998821c50581d8e3ee0223fc5740131296
Florian Hofmann <hofmann.florian@bluewin.ch>
2023-05-11 09:00:31 +0200

    05/11/2023, 09:00:31: model train

--- code/services/model-trainer/trainer.py
+++ code/services/model-trainer/trainer.py
@@ -1,10 +1,27 @@
 """
 This python file as the objective to train the model and save it on DVC
 """
-from time import sleep
+import os
+from datetime import datetime
 
+GIT_REPO:str = "https://github.com/MSE-PI/MLodImage.git"
+BRANCH_NAME: str = "train-model"
+
+def main():
+    # clone git repo and create a new branch
+    os.system(f"git clone {GIT_REPO} repo")
+    os.system("cd repo")
+    os.system(f"git checkout -b {BRANCH_NAME}")
+    # dvc pull, train model, test, ...
+    # TODO
+    with open('test.txt', 'w') as f:
+        f.write('Create a new text file!')
+    # commit and push changes
+    now = datetime.now()
+    date = now.strftime("%m/%d/%Y, %H:%M:%S")
+    os.system(f"git add . && git commit -m \"{date}: model train\"")
+    os.system("git push")
+
+# entry point
 if __name__ == '__main__':
-    print("Training model...")
-    sleep(120)
-    print("Model trained!")
-    # kubectl wait that the pod is terminated
+    main()
