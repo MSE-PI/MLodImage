a496ba14313b001e73288b36e112201f8dacf681
Florian Hofmann <hofmann.florian@bluewin.ch>
2023-05-17 16:28:37 +0200

    Update trainer script

--- code/services/model-trainer/trainer.py
+++ code/services/model-trainer/trainer.py
@@ -4,20 +4,22 @@ This python file as the objective to train the model and save it on DVC
 import os
 from datetime import datetime
 
-GIT_REPO:str = "https://github.com/MSE-PI/MLodImage.git"
+GIT_REPO: str = "https://github.com/MSE-PI/MLodImage.git"
 BRANCH_NAME: str = "train-model"
+MINIO_USR: str = os.environ["MINIO_USR"]
+MINIO_PWD: str = os.environ["MINIO_PWD"]
+
 
 def main():
     # create a new branch
-    os.system(f"git checkout -b {BRANCH_NAME}")
+    now = datetime.now()
+    os.system(f"git checkout -b {BRANCH_NAME}-{int(now.timestamp())}")
     # dvc pull, train model, test, ...
     # TODO
-    with open('test.txt', 'w') as f:
-        f.write('Create a new text file!')
     # commit and push changes
-    now = datetime.now()
-    date = now.strftime("%m/%d/%Y, %H:%M:%S")
-    os.system(f"git add . && git commit -m \"{date}: model train\" && git push –set-upstream origin {BRANCH_NAME}")
+    os.system(f"git add . && git commit -m \"{now}: model train\"")
+    os.system(f"git push --set-upstream origin {BRANCH_NAME}-{int(now.timestamp())}")
+
 
 # entry point
 if __name__ == '__main__':
