fff2f17403ace9b22f6a8bd972a3a2fe99ca0391
Florian Hofmann <hofmann.florian@bluewin.ch>
2023-05-18 14:42:40 +0200

    Trigger pipeline

--- code/services/model-trainer/trainer.py
+++ code/services/model-trainer/trainer.py
@@ -4,7 +4,6 @@ This python file as the objective to train the model and save it on DVC
 import os
 from datetime import datetime
 
-GIT_REPO: str = "https://github.com/MSE-PI/MLodImage.git"
 BRANCH_NAME: str = os.environ["TRAIN_BRANCH"]
 MINIO_USR: str = os.environ["MINIO_USR"]
 MINIO_PWD: str = os.environ["MINIO_PWD"]
@@ -15,10 +14,12 @@ def main():
     os.system(f"git checkout -b {BRANCH_NAME}")
     # dvc pull, train model, test, ...
     # TODO
-    print(BRANCH_NAME + " " + MINIO_USR + " " + MINIO_PWD)
+    # create a test file and write the timestamp
+    with open("test.txt", "w") as f:
+        f.write(str(datetime.now()))
     # commit and push changes
     now = datetime.now()
-    os.system(f"git add . && git commit -m \"{now}: model train\"")
+    os.system(f"git add . && git commit -m \"{now}: model updated\"")
     os.system(f"git push --set-upstream origin {BRANCH_NAME}-{int(now.timestamp())}")
 
 
