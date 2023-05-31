6e7006043787ce861629b122ee8715bf244f8773
Florian Hofmann <hofmann.florian@bluewin.ch>
2023-05-31 17:20:52 +0200

    Patch genre detector pre script

--- code/services/genre-detection/pre.sh
+++ code/services/genre-detection/pre.sh
@@ -4,7 +4,7 @@ cd $REPO_ROOT
 
 # Substitute the environment variables in the .dvc/config file
 
-dvc pull train
+(cd code/models/genre_detector/ && dvc pull train)
 
 cp code/models/genre_detector/model.ckpt code/services/genre-detection/model/
 
