4b24aed05a8b682b01afc2a55801bc5ad4c4bdbf
Benjamin Pasquier <benjamin.pasquier@edu.hefr.ch>
2023-06-01 09:30:14 +0200

    Update pre.sh to do envsubst before dvc pull

--- code/services/genre-detection/pre.sh
+++ code/services/genre-detection/pre.sh
@@ -3,11 +3,10 @@
 cd $REPO_ROOT
 
 # Substitute the environment variables in the .dvc/config file
+envsubst < .dvc/config > .dvc/config.local
 
 (cd code/models/genre_detector/ && dvc pull train)
 
 cp code/models/genre_detector/src/model/* code/services/genre-detection/model/
 
 cp code/models/genre_detector/params.yaml code/services/genre-detection/params.yaml
-
-envsubst < .dvc/config > .dvc/config.local
