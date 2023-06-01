4abed484b162a69c59c74816cc0e36dc3e57b22f
Benjamin Pasquier <benjamin.pasquier@edu.hefr.ch>
2023-06-01 09:57:48 +0200

    Update model loading causing genre-detector service to crash

--- code/services/genre-detection/main.py
+++ code/services/genre-detection/main.py
@@ -80,7 +80,7 @@ class MyService(Service):
         # load the model
         torch.cuda.is_available()  # Check if NVIDIA GPU is available
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = AudioCNN.load_from_checkpoint("model/model.ckpt")
+        self.model = AudioCNN.load_from_checkpoint("model/model.ckpt", map_location=self.device)
         self.model.to(self.device)
         self.model.eval()
         print("Model loaded successfully, running on device: " + str(self.device))
